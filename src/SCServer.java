bc6d9ba4e5a250d2a3fb42680a91feb91f5775e8
SJ <penguin418@naver.com>
2017-06-06 16:03:58 +0900

    구조 생성: SC Server

--- src/SCServer.java
+++ src/SCServer.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
 /**
  * Created by penguin on 17. 6. 6.
  */
-public class SCServer {
+public class SCServer{
+    public static void main(String args[]){
+        SCServer server = new SCServer();
+        SCServerGUI gui = new SCServerGUI(server);
+
+
+    }
+
+    public void SendMessage(String str){
+
+    }
 }
---
+++ src/SCServerRunnable.java
@@ -0,0 +1,5 @@
+/**
+ * Created by penguin on 17. 6. 6.
+ */
+public class SCServerRunnable {
+}
