35ef4a51d4909760701f1412ddd5ac678c5fffed
SJ <penguin418@naver.com>
2017-06-13 13:28:26 +0900

    tmp

--- src/SCServer.java
+++ src/SCServer.java
@@ -1,25 +1,64 @@
+import com.sun.corba.se.spi.activation.*;
+
+import java.io.*;
 import java.net.*;
+import java.nio.*;
+import java.nio.channels.*;
 import java.util.*;
 
 /**
  * Created by penguin on 17. 6. 6.
  */
-public class SCServer{
 
-    static int port = 4444;
+public class SCServer implements Runnable{
+    Selector selector;
 
-    public static void main(String args[]){
-        SCServer server = new SCServer();
+    public SCServer() throws IOException{
+        // non block io 생성
+        DatagramChannel channel = DatagramChannel.open();
+        channel.bind(null);
+        channel.configureBlocking(false);
 
-        Runnable runnable = new SCServerRunnable();
-        Thread thread = new Thread(runnable);
-        thread.start();
 
-        /* GUI for system management and log */
-        SCServerGUI gui = new SCServerGUI(server);
+        // 셀렉터 생성
+        selector = Selector.open();
+        // selector에 등록
+        channel.register(selector, SelectionKey.OP_ACCEPT);
+
     }
+    @Override
+    public void run() {
+        int socketOPs = SelectionKey.OP_CONNECT | SelectionKey.OP_READ | SelectionKey.OP_WRITE;
 
-    public void SendMessage(String str){
+        ByteBuffer buf = null;
+        try{
+            while(selector.select() > 0){
+                Set keys = selector.selectedKeys();
+                Iterator iter = keys.iterator();
 
+                while(iter.hasNext()){
+                    SelectionKey key = (SelectionKey)iter.next();
+                    iter.remove();
+
+                    SelectableChannel channel = key.channel();
+
+                    if(channel instanceof  ServerSocketChannel){
+                        // accept
+                        ServerSocketChannel serverChannel = (ServerSocketChannel)channel;
+                        SocketChannel socketChannel = serverChannel.accept();
+
+                        if(socketChannel == null)
+                            continue;
+
+                        // nio
+                        socketChannel.configureBlocking(false);
+
+                        socketChannel.register(selector, socketOPs);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/SCSettings.java
@@ -0,0 +1,6 @@
+/**
+ * Created by penguin on 17. 6. 13.
+ */
+public class SCSettings {
+    public static int port = 4546;
+}
