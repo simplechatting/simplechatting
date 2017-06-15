5595e20e903a0d6bbbba6d8316d29c8af12a12b2
SJ <penguin418@naver.com>
2017-06-15 21:17:13 +0900

    kasfjdlkajdfkljlsdkf

--- src/SCMessage.java
+++ src/SCMessage.java
@@ -1,4 +1,7 @@
+import Settings.*;
+
 import java.io.*;
+import java.nio.*;
 import java.nio.charset.*;
 
 /**
@@ -7,25 +10,15 @@ import java.nio.charset.*;
 public class SCMessage {
     static int version = 1;
     static Charset charset = Charset.forName("UTF-8");
+    static ByteBuffer buffer = ByteBuffer.allocate(SCSettings.datagramSize);
 
-    public int userid;
-    public int groudid;
-    public int msgType;
-    public int line;
-    public String message;
-
-    public SCMessage(){}
-    public SCMessage(int uid, int gid, int mType, String msg){
-        userid=uid;
-        groudid=gid;
-        msgType=mType;
-        message=msg;
+    public SCMessage(){
+        buffer.clear();
     }
-    public SCMessage(int uid, int gid, int mType, int l, String msg){
-        userid=uid;
-        groudid=gid;
-        msgType=mType;
-        line=l;
-        message=msg;
+
+    public SCMessage(byte[] data){
+        buffer = ByteBuffer.allocate(data.length);
+        buffer.clear();
+        buffer = ByteBuffer.wrap(data);
     }
 }
